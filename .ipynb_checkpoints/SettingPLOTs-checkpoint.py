06fdad208a03ec0a30b86a91faaa63ca2ddc091f
marvync <marvyn.inga@gmail.com>
2020-07-25 17:08:48 -0300

    cavity-ressonances

---
+++ .ipynb_checkpoints/SettingPLOTs-checkpoint.py
@@ -0,0 +1,38 @@
+from matplotlib import rcParams
+    
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = ['Arial']
+rcParams['font.size'] = '11'
+# rcParams['font.style'] = 'normal'
+# rcParams['font.weight'] = 'medium'
+# rcParams['pdf.fonttype'] = '42'
+
+# rcParams['axes.autolimit_mode'] = 'round_numbers'
+# rcParams['axes.xmargin'] = 0
+# rcParams['axes.ymargin'] = 0
+rcParams['axes.axisbelow'] = True
+
+rcParams['axes.grid'] = True
+rcParams['grid.linestyle'] = ':'
+rcParams['grid.linewidth'] = 0.8
+rcParams['grid.alpha'] = 0.5
+
+# rcParams['lines.linewidth'] = 1
+
+rcParams['xtick.direction'] = 'in'
+rcParams['ytick.direction'] = 'in'
+
+rcParams['svg.fonttype'] = 'none'
+# rcParams["figure.frameon"] = False
+# rcParams['savefig.bbox'] = 'tight'
+# rcParams['savefig.transparent'] = True
+
+# rcParams["figure.figsize"] = [4, 3]
+# rcParams['figure.autolayout'] = True
+
+def cm2inch(*tupl):
+    inch = 2.54
+    if isinstance(tupl[0], tuple):
+        return tuple(i/inch for i in tupl[0])
+    else:
+        return tuple(i/inch for i in tupl)
